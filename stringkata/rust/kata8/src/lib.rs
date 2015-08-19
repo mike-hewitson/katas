b69ee9cf0e0ca6f16f4946ef730a3d395b0949fc
mike hewitson <mike@hewitson.co.za>
2015-08-19 17:06:30 +0200

    Got to step 6 on kata8

--- stringkata/rust/kata8/src/lib.rs
+++ stringkata/rust/kata8/src/lib.rs
@@ -7,11 +7,7 @@ pub fn get_demiliter(string_numbers: &str) -> (&str, usize) {
 	let re_custom = regex::Regex::new("//(.)\n").unwrap();
 
 	if re_custom.is_match(string_numbers) {
-		delimiter = 
-			match re_custom.captures(string_numbers).unwrap().at(1) {
-				Some(x)	=> x,
-				None 	=> ""
-			};
+		delimiter = re_custom.captures(string_numbers).unwrap().at(1).unwrap();
 		string_start = 4;
 	} else {
 		delimiter = ",|\n";
@@ -32,17 +28,18 @@ pub fn add_string(string_numbers: &str) -> i32 {
 		let re = regex::Regex::new(delimiter).unwrap();
 		
 		for s in re.split(&string_numbers[string_start..]) {
-			let x:i32 = s.trim().parse()
-				.ok()
-				.expect("Not a number");
+			let x:i32 = s.trim().parse().unwrap();
 			if x < 0 {
 				list_negatives.push(x);
-			} else {
+			} else if x <= 1000 {
 				sum += x;
 			}	
 		}
 	}
-
+	if list_negatives.len() != 0 {
+		println!("{:?}", list_negatives );
+		panic!("Negatives found");
+	}
 	return sum;
 }
 
@@ -79,7 +76,12 @@ mod tests {
 		assert_eq!(21, add_string("//;\n1;2;3;4;5;6"));
 	}
 	#[test]
+	#[should_panic]
 	fn should_return_exception_when_given_any_negatives() {
 		assert_eq!(21, add_string("1,-2,3,-4,5,-6"));
+	}
+	#[test]
+	fn should_not_add_number_gt_1000() {
+		assert_eq!(19, add_string("1,1001,3,4,5,6"));
 	}
 }
