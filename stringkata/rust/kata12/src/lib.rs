66275e5b0204a4405b82c1e52304e58d06a70a04
mike hewitson <mike@hewitson.co.za>
2015-08-24 06:34:12 +0200

    Clean kata12

---
+++ stringkata/rust/kata12/src/lib.rs
@@ -0,0 +1,124 @@
+/// # Examples
+///
+/// '''
+/// use super::add_string;
+/// assert_eq!(0, add_string(""));
+/// '''
+
+extern crate regex;
+fn get_delimiters(string_numbers: &str) -> (regex::Regex, usize) {
+
+    let re_custom_single = regex::Regex::new(r"//(.)\n").unwrap();
+    let re_custom_long = regex::Regex::new(r"//\[(.*)\]\n").unwrap();
+
+    if re_custom_single.is_match(string_numbers) {
+
+        let delimiter = re_custom_single.captures(string_numbers)
+                            .unwrap()
+                            .at(1)
+                            .unwrap();
+        let re = regex::Regex::new(delimiter).unwrap();
+        return (re, 4);
+
+    } else {
+        if re_custom_long.is_match(string_numbers) {
+
+            let delimiter = re_custom_long.captures(string_numbers)
+                                .unwrap()
+                                .at(1)
+                                .unwrap();
+            let re = regex::Regex::new(delimiter).unwrap();
+            let start_position = re_custom_long.captures(string_numbers)
+                                .unwrap()
+                                .at(0)
+                                .unwrap().len();
+            return (re, start_position); 
+
+        } else {
+
+            let re = regex::Regex::new(r",|\n").unwrap();
+            return (re, 0);
+
+        }
+    }
+}
+
+
+pub fn add_string(string_numbers: &str) -> i32 {
+
+    let mut sum: i32 = 0;
+    let mut list_negatives = vec![];
+    let (re, start_position) = get_delimiters(string_numbers);
+
+    if string_numbers == "" {
+        sum = 0;
+    } else {
+
+        let mut x: i32;
+        for s in re.split(&string_numbers[start_position..]) {
+            x = s.trim().parse().unwrap();
+            if x < 0 {
+                list_negatives.push(x);
+            } else {
+                if x < 1001 {
+                    sum += x;
+                }
+            }
+        }
+    }
+
+    if list_negatives.len() == 0 {
+        return sum;
+    } else {
+        panic!("negatives not allowed {:?}", list_negatives);
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::add_string;
+
+    #[test]
+    fn should_return_zero_when_given_empty_string() {
+        assert_eq!(0, add_string(""));
+    }
+    #[test]
+    fn should_return_number_when_given_single_number() {
+        assert_eq!(0, add_string("0"));
+        assert_eq!(5, add_string("5"));
+        assert_eq!(23, add_string("23"));
+    }
+    #[test]
+    fn should_return_sum_when_given_two_numbers_comma_delim() {
+        assert_eq!(3, add_string("1,2"));
+        assert_eq!(23, add_string("10,13"));
+    }
+    #[test]
+    fn should_return_sum_when_given_n_numbers_comma_delim() {
+        assert_eq!(21, add_string("1,2,3,4,5,6"));
+    }
+    #[test]
+    fn should_return_sum_when_given_n_numbers_comma_or_nl_delim() {
+        assert_eq!(21, add_string("1,2,3\n4,5,6"));
+    }
+    #[test]
+    fn should_return_sum_when_given_n_numbers_custom_delim() {
+        assert_eq!(21, add_string("//;\n1;2;3;4;5;6"));
+    }
+    #[test]
+    #[should_panic]
+    fn should_panic_when_given_negatives() {
+        assert_eq!(9, add_string("1,-2,3,-4,5,-6"));
+    }
+    #[test]
+    fn should_return_ignore_numbers_gt_1000() {
+        assert_eq!(1001, add_string("1,1000,1001"));
+    }
+    #[test]
+    fn should_return_sum_when_given_n_numbers_custom_delim_any_length() {
+        assert_eq!(6, add_string("//[aaa]\n1aaa2aaa3"));
+    }
+
+}
+
