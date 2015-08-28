c0d2e614ee68fb4593aa4685714c10eee85956b8
mike hewitson <mike@hewitson.co.za>
2015-08-28 09:03:48 +0200

    Completed Kata18

--- stringkata/rust/kata18/src/lib.rs
+++ stringkata/rust/kata18/src/lib.rs
@@ -3,6 +3,8 @@ extern crate regex;
 fn get_delimiters(string_numbers:&str) -> (regex::Regex, usize) {
 
     let re_custom_short = regex::Regex::new(r"//(.)\n").unwrap();
+    let re_custom_long = regex::Regex::new(r"//(.*)\n").unwrap();
+    let re_unpack_delims = regex::Regex::new(r"\[(.*?)\]").unwrap();
 
     if re_custom_short.is_match(string_numbers) {
         
@@ -14,12 +16,31 @@ fn get_delimiters(string_numbers:&str) -> (regex::Regex, usize) {
  
         return (re, 4);
     } else {
+        if re_custom_long.is_match(string_numbers) {
+            
+            let mut list_delims: Vec<&str> = vec![];
+            let delimiter_string = re_custom_long.captures(string_numbers)
+                                .unwrap()
+                                .at(1)
+                                .unwrap();
+            for cap in re_unpack_delims.captures_iter(delimiter_string) {
+                list_delims.push(cap.at(1).unwrap());
+            }
+            let re = regex::Regex::new(&list_delims.connect("|")).unwrap();    
+            let start_position = re_custom_long.captures(string_numbers)
+                                .unwrap()
+                                .at(0)
+                                .unwrap()
+                                .len();
+     
+            return (re, start_position);
 
-        let re = regex::Regex::new(&",|\n").unwrap();    
+        } else {
+            let re = regex::Regex::new(&",|\n").unwrap();    
 
-        return (re, 0);
+            return (re, 0);
+        }
     }
-
 }
 
 pub fn add_string(string_numbers:&str) -> i32 {
@@ -27,14 +48,21 @@ pub fn add_string(string_numbers:&str) -> i32 {
 
     let mut sum = 0;
     let (re, start_position) = get_delimiters(&string_numbers);
+    let mut list_negatives: Vec<i32> = vec![];
 
     if string_numbers != "" {
         let mut x: i32;
         for s in re.split(&string_numbers[start_position..]) {
             x = s.trim().parse().unwrap();
-            sum += x;
-            
+            if x < 0 {
+                list_negatives.push(x);
+            } else {
+                sum += x;
+            }
         }
+    }
+    if list_negatives.len() != 0 {
+        panic!("negatives not allowed {:?}", list_negatives);
     }
     return sum;
 }
@@ -70,6 +98,19 @@ mod tests {
     #[test]
     fn should_return_sum_when_given_custom_delim() {
         assert_eq!(21, add_string("//;\n1;2;3;4;5;6"));
+    }
+    #[test]
+    #[should_panic]
+    fn should_panic_when_given_negative_numbers() {
+        assert_eq!(-3, add_string("1,-2,3,-4,5,-6"));
+    }
+    #[test]
+    fn should_return_sum_when_given_multi_char_custom_delim() {
+        assert_eq!(6, add_string("//[abc]\n1abc2abc3"));
+    }
+    #[test]
+    fn should_return_sum_when_given_n_multi_char_custom_delim() {
+        assert_eq!(6, add_string("//[abc][de]\n1abc2de3"));
     }
 }
 
