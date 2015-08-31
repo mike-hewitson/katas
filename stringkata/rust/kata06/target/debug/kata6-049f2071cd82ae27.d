a0c7a2272134756d7a1abae1524b0cd0d40c8957
mike hewitson <mike@hewitson.co.za>
2015-08-31 07:43:50 +0200

    Final Kata, removed other practice rounds

--- stringkata/rust/kata06/target/debug/kata6-049f2071cd82ae27.d
+++
@@ -1,4 +0,0 @@
-/home/mike/projects/katas/stringkata/rust/kata6/target/debug/kata6-049f2071cd82ae27: src/lib.rs
-
-/home/mike/projects/katas/stringkata/rust/kata6/target/debug/kata6-049f2071cd82ae27.d: src/lib.rs
-
--- stringkata/rust/kata06/target/debug/kata6.d
+++
@@ -1,4 +0,0 @@
-/home/mike/projects/katas/stringkata/rust/kata6/target/debug/libkata6.rlib: src/lib.rs
-
-/home/mike/projects/katas/stringkata/rust/kata6/target/debug/kata6.d: src/lib.rs
-
