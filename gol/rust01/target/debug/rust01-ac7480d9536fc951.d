294557d987892a76734247b54acc8d14b44e3104
mike hewitson <mike@hewitson.co.za>
2015-09-18 11:58:00 +0200

    Busy with kata03

---
+++ gol/rust01/target/debug/rust01-ac7480d9536fc951.d
@@ -0,0 +1,4 @@
+/home/mike/katas/gol/rust01/target/debug/rust01-ac7480d9536fc951: src/lib.rs
+
+/home/mike/katas/gol/rust01/target/debug/rust01-ac7480d9536fc951.d: src/lib.rs
+
--- stringkata/rust/kata03/src/lib.rs
+++ stringkata/rust/kata03/src/lib.rs
@@ -1,14 +1,30 @@
 extern crate regex;
 
+fn get_delim(string: &str) -> (regex::Regex, usize) {
+	let re_custom = regex::Regex::new("//(.)\n").unwrap();
+
+	if re_custom.is_match(string) {
+		let delim: &str = re_custom.captures(string)
+							.unwrap()
+							.at(1)
+							.unwrap();
+		let re = regex::Regex::new(delim).unwrap();
+		return (re, 4);
+	} else {
+		let re = regex::Regex::new(",|\n").unwrap();
+		return (re, 0);
+	}
+
+}
 
 pub fn add(string: &str) -> i32 {
 	let mut sum: i32 = 0;
-	let re = regex::Regex::new(",|\n").unwrap();
+	let (re, start_position) = get_delim(string);
 
 	if string == "" {
 		return sum;
 	} else {
-		for s in re.split(string) {
+		for s in re.split(&string[start_position..]) {
 			let x: i32 = s.trim().parse().unwrap();
 			sum += x;
 		}
@@ -40,5 +56,14 @@ mod tests {
 	#[test]
 	fn when_given_n_numbers_comma_or_nl_delim_it_should_return_their_sum() {
 		assert_eq!(21, add("1,2,3\n4,5,6"));
+	}
+	#[test]
+	fn when_given_n_numbers_custom_delim_it_should_return_their_sum() {
+		assert_eq!(21, add("//;\n1;2;3;4;5;6"));
+	}
+	#[test]
+	#[should_panic]
+	fn when_given_negative_numbers_it_should_panic() {
+		assert_eq!(-3, add("1,-2,3,-4,5,-6"));
 	}
 }
