862390c158dd471c0f23d0c1fcede19ebe95e752
mike hewitson <mike@hewitson.co.za>
2015-10-03 09:33:53 +0200

    created framework

---
+++ gol/ruby/coderetreatbase.rb
@@ -0,0 +1,40 @@
+class Point 
+
+end
+
+class World
+
+end
+
+
+
+
+
+
+describe "Point" do
+	describe ".initialize" do
+		it "should return x,y " do
+			point = Point.new(1,2)
+			expect(point.x).to eq 1
+			expect(point.y).to eq 2
+		end
+	end
+
+	end
+end
+
+describe "World" do
+	context "empty world" do
+		before do
+			world = World.new
+			world.add_point(Point.new(1,2))
+			world.add_point(Point.new(3,4))
+		end		
+		describe ".initialize" do
+			it "should initialise with a point" do
+				expect(world.points[0].x).to eq 1
+				expect(world.points[0].y).to eq 2
+			end
+		end
+	end
+end
