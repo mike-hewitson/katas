563a4af1ab5d11e7ace3b9f1f82ad0a1d8a3a42d
mike hewitson <mike@hewitson.co.za>
2015-10-06 16:51:41 +0200

    cleaned directory and made more efficient

--- gol/ruby/crcore.rb
+++ gol/ruby/crcore.rb
@@ -17,7 +17,16 @@ class World
 	end
 
 	def neighbours(cell)
-		@cells.select{|c| adjacent?(cell, c)}.count() - 1
+		# @cells.select{|c| adjacent?(cell, c)}.count() - 1
+		neighbours = 0
+		for x in cell.x - 1 .. cell.x + 1
+			for y in cell.y - 1 .. cell.y + 1
+				if @cells.include?(Coordinate.new(x,y))
+					neighbours += 1
+				end
+			end
+		end
+		return neighbours - 1
 	end
 
 	def create_candidates
@@ -33,32 +42,25 @@ class World
 		@candidates.subtract(@cells)
 	end
 
-	def print_me
-		puts "world"
-		@cells.each do |cell|
-			puts "x = #{cell.x} y = #{cell.y} neighbours #{self.neighbours(cell)}"
-		end
-		puts "-----#{@cells.size}-----"
-	end
-
 	def tick
 		new_world = Set.new
+		puts "Checking who lives #{Time.now}"
 		@cells.select{|c| self.neighbours(c).between?(2,3)}.each do |cell|
 			new_world << cell
 		end
 
+		puts "Creating candidates #{Time.now}"
 		self.create_candidates
 
+		puts "Checking who comes alive lives #{Time.now}"
 		@candidates.select{|c| self.neighbours(c) == 2}.each do |cell| #this has been reduced by one as the cell has itself as neighbours
 			new_world << cell
 		end
 
-		if @cells == new_world
-			return false
-		else
-			@cells = new_world
-			return true
-		end
+		@cells = new_world
+		
+		puts "Done #{Time.now}"
+
 	end
 
 	def size
@@ -68,6 +70,14 @@ class World
 
 	def start_world(shape)
 		@cells = shape
+	end
+
+	def print_me
+		puts "world"
+		@cells.each do |cell|
+			puts "x = #{cell.x} y = #{cell.y} neighbours #{self.neighbours(cell)}"
+		end
+		puts "-----#{@cells.size}-----"
 	end
 
 end
