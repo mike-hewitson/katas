803961eb43f56554f635343e301b076dcfe0fff4
mike hewitson <mike@hewitson.co.za>
2015-10-06 16:22:29 +0200

    Added all the definition files and changed to read then from command line

--- gol/ruby/crmain.rb
+++ gol/ruby/crmain.rb
@@ -5,15 +5,15 @@ require 'csv'
 
 WINDOW_SIZE = 800
 
-
 class GameWindow < Gosu::Window
 	def initialize
 		super WINDOW_SIZE, WINDOW_SIZE
 		self.caption = "Game of Life"
 		@color = Gosu::Color.new(0xff_ffffff)
 		@world = World.new
+		filename = "../data/" + ARGV.first + "_106.lif"
 		load_shape = Set.new
-		CSV.foreach('gunstar2_106.lif', { :col_sep => ' '}) do |row|
+		CSV.foreach(filename, { :col_sep => ' '}) do |row|
 			if row[0] != '#Life' 
 				load_shape << Coordinate.new(row[0].to_i, row[1].to_i)
 			end
@@ -32,29 +32,10 @@ class GameWindow < Gosu::Window
 			y = cell.y * ratio + 400
 			Gosu.draw_rect(x,y,ratio, ratio, @color)
 		end
-		# sleep 1
+		sleep 0.5
 		@world.tick
 	end
 end
 
-SQUARE = Set.new << Coordinate.new(1,0) << Coordinate.new(1,1) << Coordinate.new(0,0) << Coordinate.new(0,1)
-BLINKER = Set.new << Coordinate.new(1,0) << Coordinate.new(1,1) << Coordinate.new(1,2)
-TOAD = Set.new << Coordinate.new(1,0) << Coordinate.new(1,1) << Coordinate.new(1,2) << Coordinate.new(1,3) << Coordinate.new(0,1) << Coordinate.new(0,2) << Coordinate.new(0,3) << Coordinate.new(0,4)
-
-
-
-
-# world = World.new
-# world.start_world(SQUARE)
 window = GameWindow.new
 window.show
-# puts "before tick"
-# world.print_me
-
-# while world.tick	
-# 	puts "after tick"
-# 	world.print_me
-# 	sleep 2
-# end
-# puts "WORLD STATIC NOW"
-
