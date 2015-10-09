5e7cc5cc03c8e3f76ed741036798c8a3082410e9
mike hewitson <mike@hewitson.co.za>
2015-10-09 15:56:16 +0200

    ruby style guide

--- gol/ruby/Functional/crcore.rb
+++ gol/ruby/Functional/crcore.rb
@@ -1,41 +1,42 @@
-require "set"
+require 'set'
 
-Coordinate = Struct.new(:x, :y) 
-NEIGHBOUR_LIST = [[-1,-1], [-1,0], [-1,1], [0, -1], [0, 1], [1,-1], [1,0], [1,1]]
+Coordinate = Struct.new(:x, :y)
+NEIGHBOUR_LIST = [[-1, -1], [-1, 0], [-1, 1], [0, -1],
+                  [0, 1], [1, -1], [1, 0], [1, 1]]
 
 def neighbours(cell)
-	NEIGHBOUR_LIST.map{|p| Coordinate.new(p[0]+cell.x, p[1]+cell.y)}.to_set
+  NEIGHBOUR_LIST.map { |p| Coordinate.new(p[0] + cell.x, p[1] + cell.y) }
+    .to_set
 end
 
 def neighbours_count(cell, cells)
-	neighbours(cell).select{|c| cells.member?c and cell != c}.length
+  neighbours(cell).count { |c| (cells.member? c) && (cell != c) }
 end
 
 def create_candidates(cells)
-	cells.map{|cell| neighbours(cell)}.inject(&:merge).subtract(cells)
+  cells.map { |cell| neighbours(cell) }.inject(&:merge).subtract(cells)
 end
 
 def world_tick(cells)
-	(cells.select{|c| neighbours_count(c, cells).between?(2,3)} | 
-		 create_candidates(cells).select{|c| neighbours_count(c, cells) == 3}).to_set
+  (cells.select { |c| neighbours_count(c, cells).between?(2, 3) } |
+    create_candidates(cells)
+    .select { |c| neighbours_count(c, cells) == 3 }).to_set
 end
 
 def size(cells)
-	cells.map{|cell| [cell.x.abs, cell.y.abs].max}.max * 2 + 20
+  cells.map { |cell| [cell.x.abs, cell.y.abs].max }.max * 2 + 20
 end
 
 def center(cells)
-	x = cells.inject{|sum , cell| sum += cell.x}
-	y = cells.inject{|sum , cell| sum += cell.y}
-	Coordinate.new(x,y)
+  x = cells.inject(0) { |_, cell| + cell.x }
+  y = cells.inject(0) { |_, cell| + cell.y }
+  Coordinate.new(x, y)
 end
 
-def print_me(cells, name="world")
-	puts name
-	cells.each do |cell|
-		puts "x = #{cell.x} y = #{cell.y} neighbours #{neighbours_count(cell, cells)}"
-	end
-	puts "-----#{cells.size}-----"
+def print_me(cells, name = 'world')
+  puts name
+  cells.each do |c|
+    puts "x = #{c.x} y = #{c.y} neighbours #{neighbours_count(c, cells)}"
+  end
+  puts "-----#{cells.size}-----"
 end
-
-
--- gol/ruby/Functional/crmain.rb
+++ gol/ruby/Functional/crmain.rb
@@ -8,15 +8,14 @@ WINDOW_SIZE = 800
 class GameWindow < Gosu::Window
 	def initialize
 		super WINDOW_SIZE, WINDOW_SIZE
-		self.caption = "Game of Life - " + ARGV.first
+		self.caption = 'Game of Life - ' + ARGV.first
 		@color = Gosu::Color.new(0xff_ffffff)
-		filename = "../data/" + ARGV.first + "_106.lif"
 		@cells = Set.new
-		CSV.foreach(filename, { :col_sep => ' '}) do |row|
-			if row[0] != '#Life' 
-				@cells << Coordinate.new(row[0].to_i, row[1].to_i)
-			end
+		filename = '../data/' + ARGV.first + '_106.lif'
+		CSV.foreach(filename, :col_sep => ' ') do |row|
+			@cells << Coordinate.new(row[0].to_i, row[1].to_i) if row[0] != '#Life'
 		end
+		@the_middle = center(@cells)
 	end
 
 	def update
@@ -25,10 +24,9 @@ class GameWindow < Gosu::Window
 	def draw
 		thing_size = [size(@cells), 40].max
 		ratio = WINDOW_SIZE / thing_size
-		the_middle = center(@cells)
 		@cells.each do |cell|
-			x = cell.x * ratio + 400 - the_middle.x
-			y = cell.y * ratio + 400 - the_middle.y
+			x = cell.x * ratio + 400 - @the_middle.x
+			y = cell.y * ratio + 400 - @the_middle.y
 			Gosu.draw_rect(x,y,ratio, ratio, @color)
 		end
 		sleep 0.5
