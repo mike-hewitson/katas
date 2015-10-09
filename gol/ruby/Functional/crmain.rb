cf4692698cd9f3b29b48d56cc1d639f42fe4a87e
mikeyjcat <mike@hewitson.co.za>
2015-10-09 19:36:48 +0200

    used rubocop and cleaned up almost all

--- gol/ruby/Functional/crmain.rb
+++ gol/ruby/Functional/crmain.rb
@@ -6,32 +6,32 @@ require 'csv'
 WINDOW_SIZE = 800
 
 class GameWindow < Gosu::Window
-	def initialize
-		super WINDOW_SIZE, WINDOW_SIZE
-		self.caption = 'Game of Life - ' + ARGV.first
-		@color = Gosu::Color.new(0xff_ffffff)
-		@cells = Set.new
-		filename = '../data/' + ARGV.first + '_106.lif'
-		CSV.foreach(filename, :col_sep => ' ') do |row|
-			@cells << Coordinate.new(row[0].to_i, row[1].to_i) if row[0] != '#Life'
-		end
-		@the_middle = center(@cells)
-	end
+  def initialize
+    super WINDOW_SIZE, WINDOW_SIZE
+    self.caption = 'Game of Life - ' + ARGV.first
+    @color = Gosu::Color.new(0xff_ffffff)
+    @cells = Set.new
+    filename = '../data/' + ARGV.first + '_106.lif'
+    CSV.foreach(filename, :col_sep => ' ') do |row|
+      @cells << Coordinate.new(row[0].to_i, row[1].to_i) if row[0] != '#Life'
+    end
+    @the_middle = center(@cells)
+  end
 
-	def update
-	end
+  def update
+  end
 
-	def draw
-		thing_size = [size(@cells), 40].max
-		ratio = WINDOW_SIZE / thing_size
-		@cells.each do |cell|
-			x = cell.x * ratio + 400 - @the_middle.x
-			y = cell.y * ratio + 400 - @the_middle.y
-			Gosu.draw_rect(x,y,ratio, ratio, @color)
-		end
-		sleep 0.5
-		@cells = world_tick(@cells)
-	end
+  def draw
+    thing_size = [size(@cells), 40].max
+    ratio = WINDOW_SIZE / thing_size
+    @cells.each do |cell|
+      x = cell.x * ratio + 400 - @the_middle.x
+      y = cell.y * ratio + 400 - @the_middle.y
+      Gosu.draw_rect(x, y, ratio, ratio, @color)
+    end
+    sleep 0.5
+    @cells = world_tick(@cells)
+  end
 end
 
 window = GameWindow.new
--- gol/ruby/Functional/crtests.rb
+++ gol/ruby/Functional/crtests.rb
@@ -2,49 +2,49 @@ require 'set'
 require './crcore.rb'
 
 describe 'World' do
-	context 'empty' do
-		before do
-			@world = World.new
-			@world.add_point(Coordinate.new(1, 2))
-			@world.add_point(Coordinate.new(3, 4))
-		end
-		describe '.initialize' do
-			it 'should initialise with a coordinate' do
-				expect(@world.contains?(Coordinate.new(1, 2))).to eq true
-			end
-		end
-	end
-	context 'with three cells' do
-		before do
-			@world = World.new
-			@world.add_point(Coordinate.new(1, 0))
-			@world.add_point(Coordinate.new(1, 1))
-			@world.add_point(Coordinate.new(1, 2))
-		end
-		describe '.neighbours' do
-			it 'should return the number of neighbours' do
-				expect(@world.neighbours(Coordinate.new(1, 0))).to eq 1
-				expect(@world.neighbours(Coordinate.new(1, 1))).to eq 2
-				expect(@world.neighbours(Coordinate.new(1, 2))).to eq 1
-			end
-		end
-	end
-	context 'with vertical bar' do
-		before do
-			@world = World.new
-			@world.add_point(Coordinate.new(1, 0))
-			@world.add_point(Coordinate.new(1, 1))
-			@world.add_point(Coordinate.new(1, 2))
-		end
-		describe '.tick' do
-			it 'should return a horizontal bar' do
-				@world.tick
-				expect(@world.contains?(Coordinate.new(0, 1))).to eq true
-				expect(@world.contains?(Coordinate.new(1, 0))).to eq false
-				expect(@world.contains?(Coordinate.new(1, 1))).to eq true
-				expect(@world.contains?(Coordinate.new(2, 1))).to eq true
-				expect(@world.contains?(Coordinate.new(1, 2))).to eq false
-			end
-		end
-	end
+  context 'empty' do
+    before do
+      @world = World.new
+      @world.add_point(Coordinate.new(1, 2))
+      @world.add_point(Coordinate.new(3, 4))
+    end
+    describe '.initialize' do
+      it 'should initialise with a coordinate' do
+        expect(@world.contains?(Coordinate.new(1, 2))).to eq true
+      end
+    end
+  end
+  context 'with three cells' do
+    before do
+      @world = World.new
+      @world.add_point(Coordinate.new(1, 0))
+      @world.add_point(Coordinate.new(1, 1))
+      @world.add_point(Coordinate.new(1, 2))
+    end
+    describe '.neighbours' do
+      it 'should return the number of neighbours' do
+        expect(@world.neighbours(Coordinate.new(1, 0))).to eq 1
+        expect(@world.neighbours(Coordinate.new(1, 1))).to eq 2
+        expect(@world.neighbours(Coordinate.new(1, 2))).to eq 1
+      end
+    end
+  end
+  context 'with vertical bar' do
+    before do
+      @world = World.new
+      @world.add_point(Coordinate.new(1, 0))
+      @world.add_point(Coordinate.new(1, 1))
+      @world.add_point(Coordinate.new(1, 2))
+    end
+    describe '.tick' do
+      it 'should return a horizontal bar' do
+        @world.tick
+        expect(@world.contains?(Coordinate.new(0, 1))).to eq true
+        expect(@world.contains?(Coordinate.new(1, 0))).to eq false
+        expect(@world.contains?(Coordinate.new(1, 1))).to eq true
+        expect(@world.contains?(Coordinate.new(2, 1))).to eq true
+        expect(@world.contains?(Coordinate.new(1, 2))).to eq false
+      end
+    end
+  end
 end
